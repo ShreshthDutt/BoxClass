1c318c7d6539d8308006acedef036e0dbacc5b4a
ShreshthDutt <69767588+ShreshthDutt@users.noreply.github.com>
2020-09-28 12:47:57 -0400

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,16 @@
+class Ground{
+    constructer(x,y,width,height){
+        var options = {
+            isStatic:true
+        }
+        this.body = Bodies.rectangle(x ,y,width,height,options);
+        World.add(world,this.body);
+        this.height = height;
+        this.width = width;
+        }
+ display(){
+     //var pos = this.body.position; 
+     rectMode(CENTER);
+     rect(this.body.position.x,this.body.position.y,this.width,this.height);
+ }   
+}
